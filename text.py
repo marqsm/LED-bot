12a076f6d3183b2e0ad7fa90bb8f482aa2433d25
Dana <dsniezko@gmail.com>
2014-08-19 10:19:26 -0400

    current

--- text.py
+++ text.py
@@ -11,18 +11,20 @@ from PIL import Image,ImageFont,ImageDraw
 # config stuff
 screen_width = 32*2
 screen_height = 16
-target_address = '10.0.5.184:7890'
+target_address = '192.168.7.2:7890'
 frame_delay = 0.02
-font = ImageFont.truetype("/Library/Fonts/Arial.ttf",14)
+font= ImageFont.truetype("/Library/Fonts/Arial.ttf",14)
 make_rainbows = True
 color_loop = 0
 
+text_data = ["Never Graduate","I'm a teapot","does this work","hi!!!"]
+
 # if argv 
-def get_text():
-	if len(sys.argv) > 1:
-		return sys.argv[1]+"   "
+def get_text(text):
+	if len(text) > 1:
+		return text+"   "+text+"   "
 	else:
-		return "NEVER GRADUATE!   "
+		return "Hello, Hacker School!   Hello, Hacker School!   "
 
 #rainbow bg
 def rainbow_bg(c):
@@ -38,7 +40,8 @@ def generate_img():
 			all_pixels.append(pixels[x,y])
 	return all_pixels
 	
-my_text = get_text() + "   " + get_text() 	
+counter = 3
+my_text = get_text(text_data[0])
 message_width, message_height = font.getsize(my_text)
 
 im = Image.new("RGB",(screen_width,screen_height),"black")
@@ -51,10 +54,14 @@ client = opc.Client(target_address)
 print "Screen Width: %s Message Width: %s, Total Width: %s" % (screen_width,message_width,total_width) 
 
 xOffset = 0
-while True:
+arrCnt = 0
+while arrCnt < len(text_data)-1:
+
 	# Loop drawing one frame
 	if xOffset > (msg_width + screen_width):
 		xOffset -= msg_width
+		counter -= counter
+		print "new"
 	else:
 		xOffset += 1 
 
@@ -64,7 +71,7 @@ while True:
 		if color_loop > 360:
 			color_loop=0
 		else:
-			color_loop +=1
+			color_loop += 1
 	else:
 		bg = (0,0,0)
 
