dd298cd013dab4e6fd7fc47174c53abd0a0fd95f
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-25 23:15:26 -0400

    Remove unused code in ImageRenderer.

--- imageRenderer.py
+++ imageRenderer.py
@@ -6,44 +6,32 @@ from PIL import Image, ImageSequence
 class ImageRenderer:
 
     def __init__(self, screenSize):
-        self.image = None
         self.screenSize = screenSize
 
-    def convert_image(self, image):
-        # TODO: converion
-        # TODO: error, if conversion fails
-        try:
-            rgb_image = image.convert("RGB")
-        except:
-            "unable to convert image to RGBA format"
-            return False
-
-        return rgb_image
-
     def get_new_size(self, image, screen_width, screen_height):
-        img_width, img_height = image.size
         # returns new image h/w to fit screen
+        img_width, img_height = image.size
         img_ratio = img_width / img_height
         screen_ratio = screen_width / screen_height
         if screen_ratio > img_ratio:
             return (img_width * screen_height / img_height, screen_height)
+
         else:
             return (screen_width, img_height * screen_width / img_width)
 
     def fetch_image(self, url):
         print('loadImage %s' % url)
+
         try:
             img_file = urllib.urlopen(url)
             im = StringIO(img_file.read())
-            self.image = Image.open(im)
-            self.image.load()
+            image = Image.open(im)
+            image.load()
+
         except:
             print("Print fetching the image failed")
-        # TODO : fetch remote image
-        return self.image
 
-    def getFrameCount(self, image):
-        return 1
+        return image
 
     def get_queue_token(self, msgToken):
         print("get_queue_token got an msgToken")
@@ -67,6 +55,3 @@ class ImageRenderer:
         queue_token["valid"] = True
 
         return queue_token
-
-    def getImage(self):
-        return self.image
