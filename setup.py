44f268afb4115c48d62b80fc3fc26dededf293ee
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-27 12:50:32 -0400

    Initial cut of the setup.py

---
+++ setup.py
@@ -0,0 +1,82 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='LED-bot',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.5',
+
+    description='A LED application server',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/marqsm/LED-bot',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='LED LEDScape Bot Zulip',
+
+    packages=find_packages(),
+
+    # List run-time dependencies here.
+    # These are just minimal requirements.  The canonical versions are
+    # specified in the requirements.txt file.  Use that!
+    install_requires=[
+        'requests',
+        'Pillow',
+    ],
+
+    # Dependencies that are not on pypi
+    dependency_links = [
+        "https://github.com/zulip/python-zulip/archive/master.zip",
+    ],
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        # Include the font file.
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+)
