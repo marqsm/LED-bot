2bf18af15892f1cc92e13dfce7c16510403eaa61
Dana <dsniezko@gmail.com>
2014-10-17 14:03:39 -0700

    simple homepage

--- LEDBot/webRequestHandler.py
+++ LEDBot/webRequestHandler.py
@@ -1,31 +1,16 @@
-from bottle import route, request, run, template, Bottle
 import datetime
 import time
 from threading import Thread
+from os.path import dirname, exists, join
+from bottle import route, request, run, template, Bottle, static_file
 
 class WebRequestHandler:
 	"""WebRequestHandler, a RESTful API"""
 	def __init__(self,host='0.0.0.0',port=4000):
+		self.here = dirname(__file__)
 		self.www = Bottle()
 		self.callback = None
-
-		@self.www.post('/show-text/')
-		def show_text():
-			color = (0,255,0)
-			if 'message' in request.forms: 
-				if 'color' in request.forms:
-					color = tuple([int(i) for i in request.forms.color.split(",")])
-				self.send_text(request.forms.message,color)
-				print 'Your message %s! In color %s</b>' % (request.forms.message, color)
-
-		@self.www.post('/show-image/')
-		def show_image():
-			self.send_image(request.forms.url)
-			print 'image URL: %s' % (request.forms.url)
-
-		@self.www.route('/hello/<name>')
-		def index(name):
-			return template('<b>Hello {{name}}</b>!', name=name)
+		self.routes()
 
 		server = Thread(target=self.www.run,kwargs=dict(host=host, port=port));
 		server.setDaemon(True)
@@ -33,6 +18,8 @@ class WebRequestHandler:
 
 	def send_response(self, response, msg):
 		""" Send the response to a user who sent a message to us. """
+		return response["content"]
+
 
 	def listen(self,callback):
 		self.callback = callback
@@ -54,3 +41,38 @@ class WebRequestHandler:
 			'type':'image'
 		}
 		self.callback(msg, self)
+
+
+	def send_system_message(self,content):
+		msg = {
+			'type':'system',
+			'text': content
+		}
+		self.callback(msg, self)
+
+
+	def routes(self):
+
+		@self.www.route('/')
+		def index():
+		    return static_file('index.html', root=join(self.here,"www/"))
+
+		@self.www.post('/show-text/')
+		def show_text():
+			color = (0,255,0)
+			if 'message' in request.forms: 
+				if 'color' in request.forms:
+					color = tuple([int(i) for i in request.forms.color.split(",")])
+				self.send_text(request.forms.message,color)
+				print 'Your message %s! In color %s</b>' % (request.forms.message, color)
+
+		@self.www.post('/show-image/')
+		def show_image():
+			self.send_image(request.forms.url)
+			print 'image URL: %s' % (request.forms.url)
+
+		@self.www.get('/static/<filename:path>')
+		def send_static(filename):
+		    return static_file(filename, root=join(self.here,"www/"))
+
+
---
+++ LEDBot/www/form.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    ajaxFormHandler("#text-form","/show-text/",function(data) {
+      $("#reply").text(data);
+    });
+
+    ajaxFormHandler("#image-form","/show-image/",function(data) {
+      $("#reply").text(data);
+    });
+
+    $("#color-picker").spectrum({
+      color: "#007800",
+      change: function(color) {
+        var c = color.toRgb();
+        var cFormat = c.r+","+c.g+","+c.b;
+        $("#color-picker").val(cFormat);
+      }               
+    });
+
+  });
+
+  function ajaxFormHandler(selector,dest,callback) {
+    $(selector).on("submit",function(evt) {
+      evt.preventDefault();
+      $.ajax({ 
+        type: "POST",
+        url: dest,
+        data: $(selector).serialize()
+      })
+      .done(function(data) { 
+        callback(data);
+      });
+    });
+  };
